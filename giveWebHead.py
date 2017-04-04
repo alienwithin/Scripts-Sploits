9b7bfb1ab28d44238ac6792129a462364f56a4f9
Munir Njiru <munir.n.n@gmail.com>
2017-04-04 13:59:05 +0300

    added give web head python version
    Added Directory bruteforce script

---
+++ giveWebHead.py
@@ -0,0 +1,97 @@
+##
+#python giveWebHead.py -t https://example.com -w dirs.txt -i False -m GET
+##########################################
+#       Give Web Head v1.0
+#         by Alienwithin
+###########################################
+#https://example.com/foundDir1
+#https://alien-within.com/foundDir2
+#Results are written to a CSV file ie. found URLs and status code. 
+#HEAD is faster than GET ; if a server supports it then use that
+##
+import requests
+import csv
+from optparse import OptionParser
+from urlparse import urlparse
+def gwhEngine(target, wordlist, method, redirects=False):
+    error_codes_non_redir=[200,403]
+    error_codes_redir= [200,301,302,403]
+    with open(wordlist) as dirPerLine:
+		for dir in dirPerLine:
+			cleanDirName=str(dir.rstrip('\n'))
+			fullURL=urlparse(target)
+			getHostname=fullURL.netloc	
+			resultFile=open(str(getHostname)+'.csv', 'a')
+			csvWritingObject = csv.writer(resultFile)
+			if method=="HEAD" and redirects=="False":
+				gwhRequester=requests.head(target+cleanDirName)
+				gwhStatus=gwhRequester.status_code
+				if gwhStatus in error_codes_non_redir:
+					csvWritingObject.writerow( (target+cleanDirName, gwhStatus) )
+					resultFile.close()
+					print target+cleanDirName		
+			elif method=="HEAD" and redirects=="True":
+				gwhRequester=requests.head(target+cleanDirName)
+				gwhStatus=gwhRequester.status_code
+				if gwhStatus in error_codes_redir: 
+					csvWritingObject.writerow( (target+cleanDirName, gwhStatus) )
+					resultFile.close()
+					print target+cleanDirName
+			if method=="GET" and redirects=="False":
+				gwhRequester=requests.get(target+cleanDirName)
+				gwhStatus=gwhRequester.status_code
+				if gwhStatus in error_codes_non_redir: 
+					csvWritingObject.writerow( (target+cleanDirName, gwhStatus) )
+					resultFile.close()
+					print target+cleanDirName
+			elif method=="GET" and redirects=="True":
+				gwhRequester=requests.get(target)
+				gwhStatus=gwhRequester.status_code
+				if gwhStatus in error_codes_redir: 
+					csvWritingObject.writerow( (target+cleanDirName, gwhStatus) )
+					resultFile.close()
+					print target+cleanDirName
+			else:
+			 print "Error mehn"
+def giveTheWebSomeHead():
+    alienParser = OptionParser(usage="usage: %prog --help for [options]",
+                          version="%prog version : 1.0")
+    alienParser.add_option("-t", "--target",
+                      action="store",
+                      dest="target",
+                      default="http://iWannaFindyourDirectories.ws",
+                      help="Target URL")
+    alienParser.add_option("-w", "--wordlist",
+                      action="store", 
+                      dest="wordlist",
+                      default="dirlist.txt",
+                      help="Path to wordlist with directory names")
+    alienParser.add_option("-i", "--ignore-redirects",
+				  action="store", 
+				  dest="redirects",
+				  default=False,
+				  help="Path to wordlist with directory names")
+    alienParser.add_option("-m", "--method",
+				  action="store", 
+				  dest="method",
+				  default="HEAD",
+				  help="Method to use to get results i.e. HEAD or GET")				  
+    (options, args) = alienParser.parse_args()
+	
+
+    if options.target =="http://iWannaFindyourDirectories.ws" or options.target=="":
+        alienParser.error("I'm sorry there simply has to be a target for this to work")
+    elif options.wordlist=="":
+		alienParser.error("You haven't selected a wordlist")
+    else:
+		myTarget=options.target
+		if myTarget[len(myTarget)-1] != "/":
+			myTarget = myTarget + "/"
+		gwhEngine(myTarget, options.wordlist, options.method, options.redirects)
+def banner():
+      print "##########################################"
+      print "\tGive Web Head v1.0\n\t by Alienwithin\n"
+      print "##########################################"
+if __name__ == '__main__':
+	banner()
+	giveTheWebSomeHead()
